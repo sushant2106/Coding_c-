8993f9ad7b0f7941f1e8a5e52e7803edb332be8f
sushant2106 <90310743+sushant2106@users.noreply.github.com>
2022-11-26 11:57:57 +0530

    Update sqrt.cpp

--- sqrt.cpp
+++ sqrt.cpp
@@ -1,29 +1,60 @@
 #include<bits/stdc++.h>
 using namespace std;
-int binary(int n)
-{
-    int start=1;
-    int end=n-1;
+// int binary(int n)
+// {
+//     int start=1;
+//     int end=n-1;
+//     while(start<=end)
+//     {
+//         int mid=start+(end-start)/2 ;
+//         if(mid*mid==n)
+//         {
+//             return mid;
+//         }
+//         else if(mid*mid>n)
+//         {
+//             end=mid-1;
+//         }
+//         else
+//         {
+//             start=mid+1;
+//         }
+//     }
+// }
+// int main()
+// {
+//       int n=25;
+//       cout<<binary(n)<<" ";
+//     return 0;
+// }
+  long long int binarysearch(int num)
+    {
+        long long int start=0;//beacuse if the number range is high
+       long long int  end=num;
+        int ans=-1;
     while(start<=end)
     {
-        int mid=start+(end-start)/2 ;
-        if(mid*mid==n)
+          long long int mid=start +(end-start)/2 ;
+        if((mid*mid)==num)
         {
-            return mid;
+         return mid;
         }
-        else if(mid*mid>n)
+        else if((mid*mid)>num)
         {
-            end=mid-1;
+          end=mid-1;
         }
-        else
+        else if((mid*mid)<num)
         {
-            start=mid+1;
+          ans=mid;
+          start=mid+1;
         }
+  
+    
     }
-}
-int main()
-{
-      int n=25;
-      cout<<binary(n)<<" ";
-    return 0;
-}
+   return ans; 
+    }
+    long long int floorSqrt(long long int x) 
+    {
+        return binarysearch(x);
+    }
+};
